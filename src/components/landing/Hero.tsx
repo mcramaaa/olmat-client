0488dd345ad0251667ae8d73aea683f29c223576
mcramaaa <mchrama28@gmail.com>
2025-05-10 19:25:10 +0700

    fix: hero link register

--- src/components/landing/Hero.tsx
+++ src/components/landing/Hero.tsx
@@ -29,7 +29,7 @@ export function HeroSection({ user, start, end, now }: IProps) {
       >
         <p>Olimpiade Matematika</p>
       </motion.div>
-      <div className=" container mx-auto px-6 relative z-10 ">
+      <div className=" container mx-auto px-6 relative z-10 min-h-screen md:min-h-full">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
           <motion.div
             initial={{ opacity: 0, y: 20 }}
@@ -45,16 +45,15 @@ export function HeroSection({ user, start, end, now }: IProps) {
             <p className="text-lg md:text-xl text-gray-700 mb-8 max-w-xl mx-auto lg:mx-0 italic">
               {APPCONSTANT.theme}
             </p>
-            <div className="flex flex-col sm:flex-row gap-4 justify-center lg:justify-start">
+            <div className="flex flex-col sm:flex-row gap-4 justify-center lg:justify-start w-full items-center ">
               {start && end && now > start && now < end && (
                 <Link
                   href={
                     user ? ROUTES.DASHBOARD.REGISTER_PARTICIPANTS : ROUTES.LOGIN
                   }
-                  className="bg-brand hover:bg-brand/90 rounded-lg flex py-2 px-3 items-center text-white"
+                  className="bg-brand hover:bg-brand/90 rounded-lg flex py-2 px-3 items-center text-white w-fit"
                 >
                   Daftar Sekarang <ArrowRight className="ml-2 h-5 w-5" />
-                  {/* </Button> */}
                 </Link>
               )}
             </div>
@@ -108,7 +107,7 @@ export function HeroSection({ user, start, end, now }: IProps) {
         <ChevronsDown className=" w-6 text-black" />
         <ChevronsDown className=" w-6 text-brand" />
       </motion.div> */}
-      <div className="absolute w-full z-10 -bottom-1.5 flex justify-center">
+      <div className="absolute w-full z-10 -bottom-11 flex justify-center ">
         <Countdown now={now} start={start} end={end} />
       </div>
       <div className="absolute w-full bg-gradient-to-t from-white to-white/0 h-32 bottom-0 "></div>
